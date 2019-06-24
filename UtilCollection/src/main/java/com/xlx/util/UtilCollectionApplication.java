97c5d55bb536a9dbe5943d5ea68b92a3eeda2a4f
robotxin <595027271@qq.com>
2019-06-24 14:09:29 +0800

    打个底子

---
+++ UtilCollection/src/main/java/com/xlx/util/UtilCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.xlx.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilCollectionApplication.class, args);
+	}
+
+}
